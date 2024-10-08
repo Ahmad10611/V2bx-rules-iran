c5b5bf7265fd9f27af2d9efe748c7685caa16725
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2024-10-08 20:11:29 +0330

    Update update_v2bx_configs.sh

--- update_v2bx_configs.sh
+++ update_v2bx_configs.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
+# تعریف مسیر فایل‌ها
 ROUTE_FILE="/etc/V2bX/route.json"
 SING_FILE="/etc/V2bX/sing_origin.json"
-NEW_ROUTE_JSON='{...}' # محتوای فایل route.json جدید
-NEW_SING_ORIGIN_JSON='{...}' # محتوای فایل sing_origin.json جدید
+LOG_FILE="/root/v2bx_config_update.log"  # مسیر لاگر
+FILES_CHANGED=0  # متغیری برای بررسی تغییرات
 
-# بررسی و اصلاح مجوزها
+# بررسی و تنظیم مجوز فایل‌ها
 check_permissions() {
     local file_path="$1"
-    if [ "$(stat -c %a "$file_path")" != "755" ]; then
-        chmod 755 "$file_path"
-        echo "مجوز $file_path اصلاح شد."
+    local required_permissions="-rwxr-xr-x"
+    
+    current_permissions=$(stat -c "%A" "$file_path")
+    
+    if [ "$current_permissions" != "$required_permissions" ]; then
+        echo "مجوز $file_path نادرست است. تنظیم مجوز..."
+        chmod 755 "$file_path"  # تنظیم مجوز -rwxr-xr-x
+        echo "مجوز $file_path به $required_permissions تغییر یافت."
     else
         echo "مجوز $file_path صحیح است."
-    fi
-}
-
-# تابع بررسی تغییرات و به‌روزرسانی فایل‌ها
-update_file_if_needed() {
-    local file_path="$1"
-    local new_content="$2"
-    
-    if [ "$(cat $file_path)" != "$new_content" ]; then
-        echo "$new_content" > "$file_path"
-        echo "فایل $file_path به‌روزرسانی شد."
-        FILES_CHANGED=1
-    else
-        echo "فایل $file_path نیازی به به‌روزرسانی ندارد."
     fi
 }
 
@@ -34,15 +26,194 @@ update_file_if_needed() {
 check_permissions "$ROUTE_FILE"
 check_permissions "$SING_FILE"
 
+# محتوای جدید برای فایل route.json
+NEW_ROUTE_JSON='{
+    "domainStrategy": "AsIs",
+    "rules": [
+        {
+            "type": "field",
+            "outboundTag": "block",
+            "ip": [
+                "geoip:private",
+                "geoip:ir"
+            ]
+        },
+        {
+            "domain": [
+                "geosite:google"
+            ],
+            "outboundTag": "IPv4_out",
+            "type": "field"
+        },
+        {
+            "type": "field",
+            "outboundTag": "block",
+            "domain": [
+                "geosite:ir"
+            ]
+        },
+        {
+            "type": "field",
+            "outboundTag": "block",
+            "domain": [
+                "regexp:(api|ps|sv|offnavi|newvector|ulog.imap|newloc)(.map|).(baidu|n.shifen).com",
+                "regexp:(.+.|^)(360|so).(cn|com)",
+                "regexp:(Subject|HELO|SMTP)",
+                "regexp:(torrent|.torrent|peer_id=|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=)",
+                "regexp:(^.@)(guerrillamail|guerrillamailblock|sharklasers|grr|pokemail|spam4|bccto|chacuo|027168).(info|biz|com|de|net|org|me|la)",
+                "regexp:(.?)(xunlei|sandai|Thunder|XLLiveUD)(.)",
+                "regexp:(..||)(dafahao|mingjinglive|botanwang|minghui|dongtaiwang|falunaz|epochtimes|ntdtv|falundafa|falungong|wujieliulan|zhengjian).(org|com|net)",
+                "regexp:(ed2k|.torrent|peer_id=|announce|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=|magnet:|xunlei|sandai|Thunder|XLLiveUD|bt_key)",
+                "regexp:(.+.|^)(360).(cn|com|net)",
+                "regexp:(.*.||)(guanjia.qq.com|qqpcmgr|QQPCMGR)",
+                "regexp:(.*.||)(rising|kingsoft|duba|xindubawukong|jinshanduba).(com|net|org)",
+                "regexp:(.*.||)(netvigator|torproject).(com|cn|net|org)",
+                "regexp:(..||)(visa|mycard|gash|beanfun|bank).",
+                "regexp:(.*.||)(gov|12377|12315|talk.news.pts.org|creaders|zhuichaguoji|efcc.org|cyberpolice|aboluowang|tuidang|epochtimes|zhengjian|110.qq|mingjingnews|inmediahk|xinsheng|breakgfw|chengmingmag|jinpianwang|qi-gong|mhradio|edoors|renminbao|soundofhope|xizang-zhiye|bannedbook|ntdtv|12321|secretchina|dajiyuan|boxun|chinadigitaltimes|dwnews|huaglad|oneplusnews|epochweekly|cn.rfi).(cn|com|org|net|club|net|fr|tw|hk|eu|info|me)",
+                "regexp:(.*.||)(miaozhen|cnzz|talkingdata|umeng).(cn|com)",
+                "regexp:(.*.||)(mycard).(com|tw)",
+                "regexp:(.*.||)(gash).(com|tw)",
+                "regexp:(.bank.)",
+                "regexp:(.*.||)(pincong).(rocks)",
+                "regexp:(.*.||)(taobao).(com)",
+                "regexp:(.*.||)(laomoe|jiyou|ssss|lolicp|vv1234|0z|4321q|868123|ksweb|mm126).(com|cloud|fun|cn|gs|xyz|cc)",
+                "regexp:(flows|miaoko).(pages).(dev)"
+            ]
+        },
+        {
+            "type": "field",
+            "outboundTag": "block",
+            "ip": [
+                "127.0.0.1/32",
+                "10.0.0.0/8",
+                "fc00::/7",
+                "fe80::/10",
+                "172.16.0.0/12"
+            ]
+        },
+        {
+            "type": "field",
+            "outboundTag": "block",
+            "protocol": [
+                "bittorrent"
+            ]
+        }
+    ],
+    "rule_set": [
+        {
+            "tag": "geoip-ir",
+            "type": "remote",
+            "format": "binary",
+            "url": "https://raw.githubusercontent.com/Chocolate4U/Iran-sing-box-rules/rule-set/geoip-ir.srs",
+            "download_detour": "direct"
+        },
+        {
+            "tag": "geosite-ir",
+            "type": "remote",
+            "format": "binary",
+            "url": "https://raw.githubusercontent.com/Chocolate4U/Iran-sing-box-rules/rule-set/geosite-ir.srs",
+            "download_detour": "direct"
+        }
+    ]
+}'
+
+# محتوای جدید برای فایل sing_origin.json
+NEW_SING_ORIGIN_JSON='{
+  "outbounds": [
+    {
+      "tag": "direct",
+      "type": "direct",
+      "domain_strategy": "prefer_ipv4"
+    },
+    {
+      "type": "block",
+      "tag": "block"
+    }
+  ],
+  "route": {
+    "rules": [
+      {
+        "ip_is_private": true,
+        "outbound": "block"
+      },
+      {
+        "rule_set": [
+          "geosite-ir"
+        ],
+        "outbound": "direct"
+      },
+      {
+        "rule_set": [
+          "geoip-ir"
+        ],
+        "outbound": "direct"
+      },
+      {
+        "domain_regex": [
+          "(.*.||)(example-regex1|example-regex2).(com|net)"
+        ],
+        "outbound": "block"
+      },
+      {
+        "outbound": "direct",
+        "network": [
+          "udp",
+          "tcp"
+        ]
+      }
+    ],
+    "rule_set": [
+      {
+        "tag": "geoip-ir",
+        "type": "remote",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/Chocolate4U/Iran-sing-box-rules/rule-set/geoip-ir.srs",
+        "download_detour": "direct"
+      },
+      {
+        "tag": "geosite-ir",
+        "type": "remote",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/Chocolate4U/Iran-sing-box-rules/rule-set/geosite-ir.srs",
+        "download_detour": "direct"
+      }
+    ]
+  },
+  "experimental": {
+    "cache_file": {
+      "enabled": true
+    }
+  }
+}'
+
+# تابعی برای نوشتن لاگ
+log_success() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - فایل $1 با موفقیت به روز شد" >> $LOG_FILE
+}
+
+# تابعی برای بررسی تغییر و به روز رسانی فایل‌ها
+update_file_if_needed() {
+    local file_path="$1"
+    local new_content="$2"
+    
+    if ! diff <(echo "$new_content") "$file_path" >/dev/null; then
+        echo "$new_content" > "$file_path"
+        chmod 755 "$file_path"  # تنظیم مجوز -rwxr-xr-x
+        log_success "$file_path"
+        FILES_CHANGED=1  # فایل تغییر کرده است
+    else
+        echo "فایل $file_path نیازی به به‌روزرسانی ندارد."
+    fi
+}
+
 # بررسی و به‌روزرسانی فایل‌ها
-FILES_CHANGED=0
 update_file_if_needed "$ROUTE_FILE" "$NEW_ROUTE_JSON"
 update_file_if_needed "$SING_FILE" "$NEW_SING_ORIGIN_JSON"
 
-# اگر فایل‌ها تغییر کرده باشند، سرویس v2bx را restart کنید
+# اگر فایل‌ها تغییر کرده باشند، v2bx را restart کنید
 if [ $FILES_CHANGED -eq 1 ]; then
     echo "فایل‌ها تغییر کردند. v2bx در حال راه‌اندازی مجدد است..."
-    v2bx restart
+    cd /root && v2bx restart
 else
     echo "هیچ فایلی تغییر نکرده است. نیازی به راه‌اندازی مجدد نیست."
 fi
